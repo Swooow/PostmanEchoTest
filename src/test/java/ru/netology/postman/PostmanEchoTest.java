c454cdee1505d5a2f9c87df8d33c7fa7726c18fe
Swow <stepan2235@gmail.com>
2024-02-24 14:33:11 +0300

    main

---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData(){
+        var text = "Hello";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(text));
+    }
+}
